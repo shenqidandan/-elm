83d54e5ce37aae5509522de46f29672ab18f0a66
zdd <614877985@qq.com>
2020-10-27 20:14:52 +0800

    1111

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -39,7 +39,7 @@
                         :ratings="food.ratings"></ratingselect>
           <div class="rating-wrapper">
             <ul v-show="food.ratings && food.ratings.length">
-              <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings"
+              <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" :key="rating.id"
                   class="rating-item border-1px">
                 <div class="user">
                   <span class="name">{{rating.username}}</span>

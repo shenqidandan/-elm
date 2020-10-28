1b2920e8cb6cb68c11385ba7737c492d913e947b
DESKTOP-8M86HUF\admin <admin>
2020-10-28 09:49:17 +0800

    77777

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -7,7 +7,7 @@
     </transition>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
     <div class="cart-add icon-add_circle" @click.stop.prevent="addCart"></div>
-  <div>111</div>
+  <div>777</div>
   </div>
 </template>
 
